5c61e4335e9dd0856bb613c8690d9e8fbbeab36a
Radiicall <66682497+Radiicall@users.noreply.github.com>
2021-07-02 19:38:36 +0200

    Update application-icon-script.sh

--- application-icon-script.sh
+++ application-icon-script.sh
@@ -5,7 +5,7 @@ DESTDIR="/usr/share/icons/hicolor"
 ICONAME="spotify-client"
 echo
 echo ==========================================================
-echo "Which icon to change. (Default: ${ICONAME}) (CHECK .desktop FILE FOR ICON NAME AND NAME YOUR FILE APPROPRIATLY) (file extension must be .png with recommended size of >512x512)"
+echo "Which icon to change. (Default: ${ICONAME}) (CHECK .desktop FILE FOR ICON NAME AND NAME YOUR FILE APPROPRIATLY (file extension must be .png)"
 read -p "Icon name: " ICONAME
 echo
 if [ "$ICONAME" != "" ];
@@ -38,17 +38,19 @@ if [ "$DESTDIR" != "" ];
         echo "Using default destination."
 fi
 echo
-echo Resizing image...
-convert ${ICONAME}.png -resize 512x512 ${ICONAME}-512.png
-convert ${ICONAME}.png -resize 256x256 ${ICONAME}-256.png
-convert ${ICONAME}.png -resize 128x128 ${ICONAME}-128.png
-convert ${ICONAME}.png -resize 64x64 ${ICONAME}-64.png
-convert ${ICONAME}.png -resize 48x48 ${ICONAME}-48.png
-convert ${ICONAME}.png -resize 32x32 ${ICONAME}-32.png
-convert ${ICONAME}.png -resize 24x24 ${ICONAME}-24.png
-convert ${ICONAME}.png -resize 22x22 ${ICONAME}-22.png
-convert ${ICONAME}.png -resize 16x16 ${ICONAME}-16.png
+echo Resizing Image
+mkdir ${ICONDIR}/${ICONAME}
+convert ${ICONDIR}/${ICONAME}.png -resize 512x512 ${ICONDIR}/${ICONAME}/${ICONAME}-512.png
+convert ${ICONDIR}/${ICONAME}.png -resize 256x256 ${ICONDIR}/${ICONAME}/${ICONAME}-256.png
+convert ${ICONDIR}/${ICONAME}.png -resize 128x128 ${ICONDIR}/${ICONAME}/${ICONAME}-128.png
+convert ${ICONDIR}/${ICONAME}.png -resize 64x64 ${ICONDIR}/${ICONAME}/${ICONAME}-64.png
+convert ${ICONDIR}/${ICONAME}.png -resize 48x48 ${ICONDIR}/${ICONAME}/${ICONAME}-48.png
+convert ${ICONDIR}/${ICONAME}.png -resize 32x32 ${ICONDIR}/${ICONAME}/${ICONAME}-32.png
+convert ${ICONDIR}/${ICONAME}.png -resize 24x24 ${ICONDIR}/${ICONAME}/${ICONAME}-24.png
+convert ${ICONDIR}/${ICONAME}.png -resize 22x22 ${ICONDIR}/${ICONAME}/${ICONAME}-22.png
+convert ${ICONDIR}/${ICONAME}.png -resize 16x16 ${ICONDIR}/${ICONAME}/${ICONAME}-16.png
 echo Image resized successfully!
+echo
 
 if [ -d "${DESTDIR}/32x32" ];
  then
@@ -67,15 +69,15 @@ if [ -d "${DESTDIR}/32x32" ];
 	mkdir -p ${DESTDIR}/22x22/apps/
 	mkdir -p ${DESTDIR}/16x16/apps/
 	echo Copying icon files...
-	cp ${ICONDIR}/${ICONAME}-512.png ${DESTDIR}/512x512/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-256.png ${DESTDIR}/256x256/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-128.png ${DESTDIR}/128x128/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-64.png ${DESTDIR}/64x64/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-48.png ${DESTDIR}/48x48/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-32.png ${DESTDIR}/32x32/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-24.png ${DESTDIR}/24x24/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-22.png ${DESTDIR}/22x22/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-16.png ${DESTDIR}/16x16/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-512.png ${DESTDIR}/512x512/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-256.png ${DESTDIR}/256x256/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-128.png ${DESTDIR}/128x128/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-64.png ${DESTDIR}/64x64/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-48.png ${DESTDIR}/48x48/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-32.png ${DESTDIR}/32x32/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-24.png ${DESTDIR}/24x24/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-22.png ${DESTDIR}/22x22/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-16.png ${DESTDIR}/16x16/apps/${ICONAME}.png
  else
 	echo Found non-standard directory structure in ${DESTDIR}.
 	echo Starting in 5 seconds.
@@ -92,15 +94,15 @@ if [ -d "${DESTDIR}/32x32" ];
 	mkdir -p ${DESTDIR}/22/apps/
 	mkdir -p ${DESTDIR}/16/apps/
 	echo Copying icon files...
-	cp ${ICONDIR}/${ICONAME}-512.png ${DESTDIR}/512/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-256.png ${DESTDIR}/256/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-128.png ${DESTDIR}/128/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-64.png ${DESTDIR}/64/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-48.png ${DESTDIR}/48/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-32.png ${DESTDIR}/32/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-24.png ${DESTDIR}/24/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-22.png ${DESTDIR}/22/apps/${ICONAME}.png
-	cp ${ICONDIR}/${ICONAME}-16.png ${DESTDIR}/16/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-512.png ${DESTDIR}/512/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-256.png ${DESTDIR}/256/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-128.png ${DESTDIR}/128/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-64.png ${DESTDIR}/64/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-48.png ${DESTDIR}/48/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-32.png ${DESTDIR}/32/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-24.png ${DESTDIR}/24/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-22.png ${DESTDIR}/22/apps/${ICONAME}.png
+	cp ${ICONDIR}/${ICONAME}/${ICONAME}-16.png ${DESTDIR}/16/apps/${ICONAME}.png
 fi
 echo ==========================================================
 echo
