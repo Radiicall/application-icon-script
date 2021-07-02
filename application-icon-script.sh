d768523db4ffedb7c259c3e7c0cbc9f989780cd8
Radiicall <66682497+Radiicall@users.noreply.github.com>
2021-07-02 19:19:43 +0200

    Update application-icon-script.sh

--- application-icon-script.sh
+++ application-icon-script.sh
@@ -5,7 +5,7 @@ DESTDIR="/usr/share/icons/hicolor"
 ICONAME="spotify-client"
 echo
 echo ==========================================================
-echo "Which icon to change. (Default: ${ICONAME}) (CHECK .desktop FILE FOR ICON NAME AND NAME YOUR FILES APPROPRIATLY EX: ${ICONAME}-16.png)"
+echo "Which icon to change. (Default: ${ICONAME}) (CHECK .desktop FILE FOR ICON NAME AND NAME YOUR FILE APPROPRIATLY (file extension must be .png)"
 read -p "Icon name: " ICONAME
 echo
 if [ "$ICONAME" != "" ];
@@ -38,6 +38,17 @@ if [ "$DESTDIR" != "" ];
         echo "Using default destination."
 fi
 echo
+echo Resizing Image
+convert ${ICONAME}.png -resize 512x512 ${ICONAME}-512.png
+convert ${ICONAME}.png -resize 256x256 ${ICONAME}-256.png
+convert ${ICONAME}.png -resize 128x128 ${ICONAME}-128.png
+convert ${ICONAME}.png -resize 64x64 ${ICONAME}-64.png
+convert ${ICONAME}.png -resize 48x48 ${ICONAME}-48.png
+convert ${ICONAME}.png -resize 32x32 ${ICONAME}-32.png
+convert ${ICONAME}.png -resize 24x24 ${ICONAME}-24.png
+convert ${ICONAME}.png -resize 22x22 ${ICONAME}-22.png
+convert ${ICONAME}.png -resize 16x16 ${ICONAME}-16.png 
+
 if [ -d "${DESTDIR}/32x32" ];
  then
  	echo Normal directory structure found.
