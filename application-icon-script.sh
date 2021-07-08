9498e2bb7dfebfb59f476698da8b991eb398327f
Radiicall <66682497+Radiicall@users.noreply.github.com>
2021-07-08 16:48:57 +0200

    added more variables made it detect root

--- application-icon-script.sh
+++ application-icon-script.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 echo "Application Icon Script: Starting.
 "
-ICONDIR="${PWD}"
-DESTDIR="/usr/share/icons/hicolor"
-ICONAME="spotify-client"
+if [ `whoami` != root ];
+ then
+	DEFDESTDIR="${HOME}/.local/share/icons/hicolor"
+ else
+	DEFDESTDIR="/usr/share/icons/hicolor"
+fi
+DEFICONDIR="${PWD}"
+DEFICONAME="spotify-client"
 echo ==========================================================
-echo "Which icon to change. (Default: ${ICONAME}) (CHECK .desktop FILE FOR ICON NAME AND NAME YOUR FILE APPROPRIATLY (file extension must be .png)"
+echo "Which icon to change. (Default: ${DEFICONAME}) (CHECK .desktop FILE FOR ICON NAME AND NAME YOUR FILE APPROPRIATLY) (file extension must be .png with recommended size of >512x512)"
 read -p "Icon name: " ICONAME
 echo
 	if [ "$ICONAME" != "" ];
 	 then
 	        echo "Icon name changed to "${ICONAME}"."
 	 else
-	        ICONAME="spotify-client"
+	        ICONAME="${DEFICONAME}"
 	        echo "Using default icon name."
 	fi
 echo "
@@ -23,33 +28,35 @@ echo
 	 then
 		echo "Icon directory changed to "${ICONDIR}"."
 	 else
-		ICONDIR="${PWD}"
+		ICONDIR="${DEFICONDIR}"
 		echo "Using default Icon directory."
 	fi
 
 echo "
-Where the icons should be moved to (Default: ${DESTDIR})"
+Where the icons should be moved to (Default: ${DEFDESTDIR})"
 read -p "Icon Destination: " DESTDIR
 echo
 	if [ "$DESTDIR" != "" ];
 	 then
 	        echo "Destination directory changed to "${DESTDIR}"."
 	 else
-	        DESTDIR="/usr/share/icons/hicolor"
+	        DESTDIR="${DEFDESTDIR}"
 	        echo "Using default destination."
 	fi
 echo "
-Resizing Image"
-	mkdir ${ICONDIR}/${ICONAME}
-		convert ${ICONDIR}/${ICONAME}.png -resize 512x512 ${ICONDIR}/${ICONAME}/${ICONAME}-512.png
-		convert ${ICONDIR}/${ICONAME}.png -resize 256x256 ${ICONDIR}/${ICONAME}/${ICONAME}-256.png
-		convert ${ICONDIR}/${ICONAME}.png -resize 128x128 ${ICONDIR}/${ICONAME}/${ICONAME}-128.png
-		convert ${ICONDIR}/${ICONAME}.png -resize 64x64 ${ICONDIR}/${ICONAME}/${ICONAME}-64.png
-		convert ${ICONDIR}/${ICONAME}.png -resize 48x48 ${ICONDIR}/${ICONAME}/${ICONAME}-48.png
-		convert ${ICONDIR}/${ICONAME}.png -resize 32x32 ${ICONDIR}/${ICONAME}/${ICONAME}-32.png
-		convert ${ICONDIR}/${ICONAME}.png -resize 24x24 ${ICONDIR}/${ICONAME}/${ICONAME}-24.png
-		convert ${ICONDIR}/${ICONAME}.png -resize 22x22 ${ICONDIR}/${ICONAME}/${ICONAME}-22.png
-		convert ${ICONDIR}/${ICONAME}.png -resize 16x16 ${ICONDIR}/${ICONAME}/${ICONAME}-16.png
+The resized images will be put in ${PWD}/${ICONAME}/
+Resizing Image in 5 seconds..."
+sleep 5
+	mkdir ${PWD}/${ICONAME}
+		convert ${ICONDIR}/${ICONAME}.png -resize 512x512 ${PWD}/${ICONAME}/${ICONAME}-512.png
+		convert ${ICONDIR}/${ICONAME}.png -resize 256x256 ${PWD}/${ICONAME}/${ICONAME}-256.png
+		convert ${ICONDIR}/${ICONAME}.png -resize 128x128 ${PWD}/${ICONAME}/${ICONAME}-128.png
+		convert ${ICONDIR}/${ICONAME}.png -resize 64x64 ${PWD}/${ICONAME}/${ICONAME}-64.png
+		convert ${ICONDIR}/${ICONAME}.png -resize 48x48 ${PWD}/${ICONAME}/${ICONAME}-48.png
+		convert ${ICONDIR}/${ICONAME}.png -resize 32x32 ${PWD}/${ICONAME}/${ICONAME}-32.png
+		convert ${ICONDIR}/${ICONAME}.png -resize 24x24 ${PWD}/${ICONAME}/${ICONAME}-24.png
+		convert ${ICONDIR}/${ICONAME}.png -resize 22x22 ${PWD}/${ICONAME}/${ICONAME}-22.png
+		convert ${ICONDIR}/${ICONAME}.png -resize 16x16 ${PWD}/${ICONAME}/${ICONAME}-16.png
 	echo "Image resized successfully!
 "
 
@@ -70,15 +77,15 @@ if [ -d "${DESTDIR}/32x32" ];
 		mkdir -p ${DESTDIR}/22x22/apps/
 		mkdir -p ${DESTDIR}/16x16/apps/
 	echo Copying icon files...
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-512.png ${DESTDIR}/512x512/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-256.png ${DESTDIR}/256x256/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-128.png ${DESTDIR}/128x128/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-64.png ${DESTDIR}/64x64/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-48.png ${DESTDIR}/48x48/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-32.png ${DESTDIR}/32x32/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-24.png ${DESTDIR}/24x24/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-22.png ${DESTDIR}/22x22/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-16.png ${DESTDIR}/16x16/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-512.png ${DESTDIR}/512x512/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-256.png ${DESTDIR}/256x256/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-128.png ${DESTDIR}/128x128/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-64.png ${DESTDIR}/64x64/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-48.png ${DESTDIR}/48x48/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-32.png ${DESTDIR}/32x32/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-24.png ${DESTDIR}/24x24/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-22.png ${DESTDIR}/22x22/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-16.png ${DESTDIR}/16x16/apps/${ICONAME}.png
  else
 	echo Found non-standard directory structure in ${DESTDIR}.
 	echo Starting in 5 seconds.
@@ -95,15 +102,15 @@ if [ -d "${DESTDIR}/32x32" ];
 		mkdir -p ${DESTDIR}/22/apps/
 		mkdir -p ${DESTDIR}/16/apps/
 	echo Copying icon files...
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-512.png ${DESTDIR}/512/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-256.png ${DESTDIR}/256/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-128.png ${DESTDIR}/128/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-64.png ${DESTDIR}/64/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-48.png ${DESTDIR}/48/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-32.png ${DESTDIR}/32/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-24.png ${DESTDIR}/24/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-22.png ${DESTDIR}/22/apps/${ICONAME}.png
-		cp ${ICONDIR}/${ICONAME}/${ICONAME}-16.png ${DESTDIR}/16/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-512.png ${DESTDIR}/512/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-256.png ${DESTDIR}/256/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-128.png ${DESTDIR}/128/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-64.png ${DESTDIR}/64/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-48.png ${DESTDIR}/48/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-32.png ${DESTDIR}/32/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-24.png ${DESTDIR}/24/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-22.png ${DESTDIR}/22/apps/${ICONAME}.png
+		cp ${PWD}/${ICONAME}/${ICONAME}-16.png ${DESTDIR}/16/apps/${ICONAME}.png
 fi
 echo ==========================================================
 echo "
